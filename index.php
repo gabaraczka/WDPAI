5b7e7195c28ded9ce90679c26f19c01f2b69af3f
Gabriela Rączka <gabriela.raczkaa@gmail.com>
2025-03-02 12:00:48 +0100

    Add basic config and view

--- index.php
+++ index.php
@@ -1,3 +1,53 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+    <style>
+        body {
+            background-color: #282c34;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: Arial, sans-serif;
+            color: white;
+        }
 
-echo "<h1>Hello world 💪</h1>";
+        h1 {
+            font-size: 3em;
+            animation: fadeIn 2s ease-in-out, colorChange 5s infinite alternate;
+        }
+
+        @keyframes fadeIn {
+            from {
+                opacity: 0;
+            }
+            to {
+                opacity: 1;
+            }
+        }
+
+        @keyframes colorChange {
+            0% {
+                color: #ff6f61;
+            }
+            25% {
+                color: #ffcc00;
+            }
+            50% {
+                color:rgb(88, 5, 75);
+            }
+            75% {
+                color: #00bcd4;
+            }
+            100% {
+                color: #9c27b0;
+            }
+        }
+    </style>
+</head>
+<body>
+<?php echo "<h1>Hello world 💪</h1>"; ?>
+</body>
+</html>
